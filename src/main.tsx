7b213830f0841ac1003aa8307a8ee9f47cb5581f
Adab-code <alfaro.aaron.dev@gmail.com>
2026-09-22 23:12:28 -0500

    ♻️ Usar logoMin.ico como favicon de la página

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import faviconUrl from './assets/icon/logoMin.ico'
+
+const favicon = document.createElement('link')
+favicon.rel = 'icon'
+favicon.href = faviconUrl
+document.head.appendChild(favicon)
 
 const rootElement = document.getElementById('root')
 if (!rootElement) throw new Error('Root element not found')
